0d848b391a5c3f7b304157a04099658f55b15aa0
Jon Haase <haase1020@gmail.com>
2020-05-08 14:22:52 -0400

    add favicon

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 const ToDoList = (props) => {
-    console.log (props)
-    const handleToggle = (id) => {
-        props.dispatch({type: "COMPLETED", id: id})
-    }
+  const handleToggle = (id) => {
+    props.dispatch({ type: "COMPLETED", id: id });
+  };
 
-    return(
-        <div>
-            {props.state.map(todo => {
-                return (
-                    <div key={todo.id} 
-                    className={`todo ${todo.completed ? "completed" : ""}`}
-                    onClick={() => handleToggle(todo.id)}>
-                    <p>{todo.item}</p>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div>
+      {props.state.map((todo) => {
+        return (
+          <div
+            key={todo.id}
+            className={`todo ${todo.completed ? "completed" : ""}`}
+            onClick={() => handleToggle(todo.id)}
+          >
+            <p>{todo.item}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default ToDoList;
